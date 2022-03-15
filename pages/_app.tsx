c8f8a1f2c06529f8b168a3763075da710b1aac6b
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-03-15 15:25:07 +0300

    config | styles - setup styled components
    setup styled components to work with nextjs and typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    powderWhite: "#FFFDF9",
+    persianGreen: "#06B49A",
+    lightBlue: "#AFDBD2",
+    onyx: "#36313D",
+  },
+  fonts: ["sans-serif", "Roboto"],
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em",
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

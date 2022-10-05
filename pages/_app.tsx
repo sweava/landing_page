f88fd198ff7ca7802eeca71c020f85fcb4832996
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-10-05 08:02:10 +0300

    fix(styles): move the globalstyles inside the theme-provider
    This ensures that the theme object is accessible inside the definitions
    of globalstyles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,20 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Raleway', sans-serif;
+    font-family: ${({ theme }) => theme.fonts.primary};
+    background-color: ${({ theme }) => theme.colors.primary};
+    h1, h2, p, small {
+      margin: 0;
+      padding: 0;
+    }
   }
 `;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
